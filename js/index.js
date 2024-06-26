49a80c5975174ecfe1ffcd998a38057c0b10176b
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-06-26 12:55:20 +0500

    project changed

--- js/index.js
+++ js/index.js
@@ -10,6 +10,10 @@ const select3 = document.querySelector(".select .uzbek");
 const h3Text = document.querySelectorAll(".title h3");
 const pText = document.querySelectorAll(".title p");
 const buttonText = document.querySelectorAll(".title button");
+const button1 = document.querySelector(".header_buttons .head_button1");
+const button2 = document.querySelector(".header_buttons .head_button2");
+const button3 = document.querySelector(".header_buttons .head_button3");
+const h4 = document.querySelectorAll(".header h4");
 
 check.onclick = mode;
 function mode() {
@@ -22,15 +26,68 @@ function mode() {
   }
 }
 
-select2.onclick = language;
+button2.addEventListener("click", function () {
+  h3Text[0].textContent = "Раскройте силу творчества c Figma!";
+  pText[0].textContent = "Готовы улучшить свою дизайнерскую игру?";
+  buttonText[0].textContent = "Попробуйте Фигму";
+  h3Text[1].textContent = "Раскройте силу творчества c Figma!";
+  pText[1].textContent = "Готовы улучшить свою дизайнерскую игру?";
+  buttonText[1].textContent = "Попробуйте Фигму";
 
-function language() {
-  if(select2.click) {
-    h3Text[0].innerHTML = "Раскройте силу творчества c Figma!";
-    pText[0].innerHTML = "Готовы улучшить свою дизайнерскую игру?";
-    buttonText[0].innerHTML = "Попробуйте Фигму прямо сейчас";
-    h3Text[1].innerHTML = "Раскройте силу творчества c Figma!";
-    pText[1].innerHTML = "Готовы улучшить свою дизайнерскую игру?";
-    buttonText[1].innerHTML = "Попробуйте Фигму прямо сейчас";
-  }
-}
+  h3Text[0].style.fontFamily = "Inter";
+  pText[0].style.fontFamily = "Inter";
+  buttonText[0].style.fontFamily = "Inter";
+
+  h3Text[1].style.fontFamily = "Inter";
+  pText[1].style.fontFamily = "Inter";
+  buttonText[1].style.fontFamily = "Inter";
+
+  h4[0].textContent = "ТЕМНЫЙ";
+  h4[1].textContent = "СВЕТОВОЙ";
+  h4[0].style.fontFamily = "Inter";
+  h4[1].style.fontFamily = "Inter";
+});
+
+button3.addEventListener("click", function () {
+  h3Text[0].textContent = "Figma bilan ijodkorlik kuchini oching!";
+  pText[0].textContent = "Dizayn o'yiningizni oshirishga tayyormisiz?";
+  buttonText[0].textContent = "Figmani sinab ko'ring";
+  h3Text[1].textContent = "Figma bilan ijodkorlik kuchini oching!";
+  pText[1].textContent = "Dizayn o'yiningizni oshirishga tayyormisiz?";
+  buttonText[1].textContent = "Figmani sinab ko'ring";
+
+  h3Text[0].style.fontFamily = "Inter";
+  pText[0].style.fontFamily = "Inter";
+  buttonText[0].style.fontFamily = "Inter";
+
+  h3Text[1].style.fontFamily = "Inter";
+  pText[1].style.fontFamily = "Inter";
+  buttonText[1].style.fontFamily = "Inter";
+
+  h4[0].textContent = "Qora rejim";
+  h4[1].textContent = "Yoruq rejim";
+  h4[0].style.fontFamily = "Inter";
+  h4[1].style.fontFamily = "Inter";
+});
+
+button1.addEventListener("click", function () {
+  h3Text[0].textContent = "Unlock the Power of Creativity with Figma!";
+  pText[0].textContent = "Ready to elevate your design game?";
+  buttonText[0].textContent = "Try Figma now";
+  h3Text[1].textContent = "Unlock the Power of Creativity with Figma!";
+  pText[1].textContent = "Ready to elevate your design game?";
+  buttonText[1].textContent = "Try Figma now";
+
+  h3Text[0].style.fontFamily = "Inter";
+  pText[0].style.fontFamily = "Inter";
+  buttonText[0].style.fontFamily = "Inter";
+
+  h3Text[1].style.fontFamily = "Inter";
+  pText[1].style.fontFamily = "Inter";
+  buttonText[1].style.fontFamily = "Inter";
+
+  h4[0].textContent = "DARK MODE";
+  h4[1].textContent = "LIGHT MODE";
+  h4[0].style.fontFamily = "Inter";
+  h4[1].style.fontFamily = "Inter";
+});
